8ca4c186d46feb38265b9e461cc9dd25ebb0f16d
gjoranv <gv@verizonmedia.com>
2020-02-03 13:48:50 +0100

    Import code and test for Cloudwatch emitter.

---
+++ cloudwatch/vespa_cloudwatch_emitter.py
@@ -0,0 +1,158 @@
+import boto3
+import json
+import logging
+import requests
+from requests.exceptions import Timeout, HTTPError, InvalidURL, ConnectionError
+
+logging.basicConfig(format='%(asctime)s\t%(levelname)s\t%(message)s',
+                    level=logging.INFO)
+log = logging.getLogger('vespa_cloudwatch_emitter')
+
+
+class VespaCloudwatchEmitter:
+
+    VESPA_URL = 'http://my-host:8080/metrics/v2/values'
+    NAMESPACE = 'my-cloudwatch-namespace'
+    CHUNK_SIZE = 20
+
+    def run(self):
+        log.info('Retrieving Vespa metrics from {}'.format(self.VESPA_URL))
+        try:
+            metrics_json = self._get_metrics_json(self.VESPA_URL)
+            log.debug("json: {}".format(metrics_json))
+            if 'nodes' not in metrics_json:
+                log.warning("No 'nodes' in metrics json")
+                return
+            metric_data = self.all_metric_data_for_response(metrics_json)
+            self._emit_metric_data(metric_data)
+        except Timeout as e:
+            log.warning("Timed out connecting to Vespa's metrics api: {}".format(e))
+        except (HTTPError, InvalidURL, ConnectionError) as e:
+            log.warning("Could not connect to Vespa's metrics api: {}".format(e))
+        except Exception as e:
+            log.warning("Unexpected error: {}".format(e))
+
+    def _emit_metric_data(self, metric_data):
+        """ Emit metric data in chunks of max CHUNK_SIZE metrics each
+        """
+        cloudwatch = boto3.client('cloudwatch')
+        log.info("Emitting {} metrics in chunks of max {}".format(len(metric_data), self.CHUNK_SIZE))
+        metric_data_chunks = self.split_list(metric_data, self.CHUNK_SIZE)
+        for chunk in metric_data_chunks:
+            log.info("Emitting chunk with {} metrics".format(len(chunk)))
+            response = self._emit_to_cloudwatch(cloudwatch, chunk)
+            log.info(response)
+
+    def _emit_to_cloudwatch(self, client, metric_data):
+        response = client.put_metric_data(MetricData=metric_data, Namespace='VESPA/gv-test')
+        return response
+
+    def _get_metrics_json(self, url):
+        """ Send rest request to metrics api and return the response as JSON
+        """
+        log.info("Sending request to {}".format(url))
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+
+    def all_metric_data_for_response(self, response_json):
+        metric_data = []
+        for nodes_elem in response_json['nodes']:
+            # Using dict.get() to avoid exception if non-existent
+            log.info("Parsing metrics from node {}".format(nodes_elem.get('hostname')))
+
+            metric_data.extend(self._metric_data_for_node_node(nodes_elem))
+            metric_data.extend(self._metric_data_for_node_services(nodes_elem))
+        return metric_data
+
+    def _metric_data_for_node_node(self, nodes_elem):
+        """
+        Returns a list of MetricsData for the 'node' (= node metrics) element
+        for a Vespa node (here represented by nodes_elem).
+        """
+        if 'node' not in nodes_elem:
+            log.info("No node metrics for node {} (this is expected for self-hosted Vespa)."
+                         .format(nodes_elem.get('hostname')))
+            return []
+        return self._metric_data_for_service_or_node(nodes_elem['node'])
+
+    def _metric_data_for_node_services(self, nodes_elem):
+        """
+        Returns a list of MetricsData for all elements in the 'services'
+        list for a Vespa node (here represented by nodes_elem).
+        """
+        if 'services' not in nodes_elem:
+            log.warning("No services for node {}".format(nodes_elem.get('hostname')))
+            return []
+
+        metric_data = []
+        for service in nodes_elem['services']:
+            metric_data.extend(self._metric_data_for_service_or_node(service))
+        return metric_data
+
+    def _metric_data_for_service_or_node(self, service_or_node):
+        """
+        Returns a list of MetricsData for all elements in the 'metrics' json
+        list of a 'nodes' element's 'node' element or one of the elements
+        in a node's 'services' list.
+        """
+        if 'metrics' not in service_or_node:
+            return []
+        metric_data = []
+        for metrics_elem in service_or_node['metrics']:
+            metric_data.extend(self._get_metrics_with_dimensions(metrics_elem))
+        return metric_data
+
+    def _get_metrics_with_dimensions(self, metrics_elem):
+        """
+        Returns a list of MetricsData for one element in a 'metrics' json list.
+        """
+        if 'values' not in metrics_elem:
+            return []
+        metric_data = []
+        dimensions = self._get_dimensions(metrics_elem)
+        for name, value in metrics_elem['values'].items():
+            metric_data.append({'MetricName': name,
+                                'Value': value,
+                                'Unit': 'None',
+                                'Dimensions': dimensions})
+        return metric_data
+
+    def _get_dimensions(self, metrics):
+        dimensions = []
+        if 'dimensions' in metrics:
+            dimensions_json = metrics['dimensions']
+            for dim, dim_val in dimensions_json.items():
+                dimensions.append({'Name': dim,
+                                   'Value': dim_val})
+        return dimensions
+
+    def split_list(self, lst, chunk_size):
+        """
+        Splits the given list into chunks and returns the result as a list of lists/chunks
+        """
+        return list(self._chunk(lst, chunk_size))
+
+    def _chunk(self, lst, chunk_size):
+        """ Splits list 'lst' in chunks of max n elements.
+        """
+        res = []
+        for item in lst:
+            res.append(item)
+            if len(res) >= chunk_size:
+                yield res
+                res = []
+        if res:
+            yield res  # the remaining portion of the original list
+
+
+def lambda_handler(event, context):
+    VespaCloudwatchEmitter().run()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Done.')
+    }
+
+
+if __name__ == '__main__':
+    VespaCloudwatchEmitter().run()
